f02e1b0cc505e2d3114453626a62626118be9c9a
Andrew Lima <andrew@arctek.co.za>
2021-09-09 13:00:23 +0200

    Localize PMPro Zapier
    Localize PMPro Zapier.

--- pmpro-zapier.php
+++ pmpro-zapier.php
@@ -5,8 +5,9 @@ Plugin URI: https://www.paidmembershipspro.com/add-ons/pmpro-zapier/
 Description: Integrate activity on your membership site with thousands of other apps via Zapier.
 Author: Paid Memberships Pro
 Author URI: https://www.paidmembershipspro.com
-Version: 1.1.1
+Version: 1.2.0
 Text Domain: pmpro-zapier
+Domain Path: /languages
 */
 
 // Includes.
@@ -17,3 +18,13 @@ define( 'PMPRO_ZAPIER_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 require_once PMPRO_ZAPIER_DIR . '/includes/admin.php';
 require_once PMPRO_ZAPIER_DIR . '/includes/class-pmpro-zapier.php';
 require_once PMPRO_ZAPIER_DIR . '/includes/settings.php';
+
+/**
+ * Load the localization function to make strings translatable.
+ * 
+ * @since 1.2.0
+ */
+function pmproz_load_textdomain(){
+	load_plugin_textdomain( 'pmpro-zapier', false, basename( dirname( __FILE__ ) ) . '/languages' ); 
+}
+add_action( 'init', 'pmproz_load_textdomain' );
